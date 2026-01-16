a14b42edb75b8ccf3201dc09320613833513656f
Neyaskin <neyaskin.kv@gmail.com>
2026-01-16 18:30:31 +0300

    Add files via upload

---
+++ Controllers/PageController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspKT1.Controllers
+{
+    [Route("Page")]
+    public class PageController : Controller
+    {
+        [HttpGet("Welcome")]
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        [HttpGet("Greet/{name}")]
+        public IActionResult Greet(string name)
+        {
+            return View(model: name);
+        }
+
+
+        [HttpGet("Edit")]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost("Result")]
+        public IActionResult Result(string message)
+        {
+            return View(model: message);
+        }
+    }
+}
